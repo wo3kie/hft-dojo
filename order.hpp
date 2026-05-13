e99ba303e0c488ac34efab5d96600a555d2a7ce1
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-13 19:17:17 +0200

    Update order.hpp

---
+++ order.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+/*
+ * Website:
+ *      HFTDojo https://github.com/wo3kie/hft-dojo
+ *
+ * Author:
+ *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
+ */
+
+#include <cstdint>
+
+struct Order
+{
+  int32_t id;
+  int32_t qty;
+};
