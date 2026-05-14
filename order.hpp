58ef574c07935c03e6571d67f8db18c9c5ba5209
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-14 21:02:51 +0200

    Update order.hpp

--- order.hpp
+++ order.hpp
@@ -8,6 +8,8 @@
  *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
  */
 
+#include <iostream>
+
 #include "./common.hpp"
 
 struct Order
@@ -15,3 +17,18 @@ struct Order
   OrderId id;
   Qty qty;
 };
+
+inline bool operator==(const Order& lhs, const Order& rhs)
+{
+  return lhs.id == rhs.id && lhs.qty == rhs.qty;
+}
+
+inline bool operator!=(const Order& lhs, const Order& rhs)
+{
+  return !(lhs == rhs);
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Order& order)
+{
+  return os << "Order{id=" << order.id << ", qty=" << order.qty << "}";
+}
