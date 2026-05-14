11c576e6c7236944b6fbe21ce0f0af8ca2603fac
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-14 19:10:39 +0200

    Update likely.hpp

--- likely.hpp
+++ likely.hpp
@@ -8,5 +8,5 @@
  *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
  */
 
-#define LIKELY(x)   (__builtin_expect(!!(x), 1))
-#define UNLIKELY(x) (__builtin_expect(!!(x), 0))
+#define LIKELY(x) (__builtin_expect(! ! (x), 1))
+#define UNLIKELY(x) (__builtin_expect(! ! (x), 0))
