7ea444565fa14a656b4dcee2852f8749e3589ec3
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-14 19:10:04 +0200

    Update demangle.hpp

--- demangle.hpp
+++ demangle.hpp
@@ -8,20 +8,21 @@
  *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
  */
 
-#include <cxxabi.h>
 #include <cstdlib>
+#include <cxxabi.h>
 #include <string>
 
-inline std::string demangle(const char* mangled_name) {
-    int status = 0;
-    char* demangled = abi::__cxa_demangle(mangled_name, nullptr, nullptr, &status);
+inline std::string demangle(const char* mangled_name)
+{
+  int status = 0;
+  char* demangled = abi::__cxa_demangle(mangled_name, nullptr, nullptr, &status);
 
-    if (status != 0) {
-        return mangled_name;
-    }
+  if(status != 0) {
+    return mangled_name;
+  }
 
-    std::string result(demangled);
-    std::free(demangled);
+  std::string result(demangled);
+  std::free(demangled);
 
-    return result;
+  return result;
 }
