dc266a934447e119ba095e628161f840415d2da0
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-14 19:10:33 +0200

    Update gdb_utils.py

---
+++ gdb_utils.py
@@ -0,0 +1,28 @@
+import gdb # type: ignore
+
+class PrintFlatList(gdb.Command):
+    def __init__(self):
+        super(PrintFlatList, self).__init__("print_flat_list", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        val = gdb.parse_and_eval(arg)
+        head = int(val["_head"])
+        size = val.type.template_argument(0)
+        storage = val["_storage"]["_M_elems"]
+
+        N = 4
+        elems = []
+
+        while head != -1:
+            node = storage[head]
+            elems.append(str(node["value"]))
+            head = int(node["next"])
+
+        if len(elems) <= 2 * N:
+            shown = elems
+        else:
+            shown = elems[:N] + ["..."] + elems[-N:]
+
+        print(f"FlatList<{size}, {val.type.template_argument(1)}> [ {", ".join(shown)} ]")
+
+PrintFlatList()
