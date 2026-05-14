cbe2dcbcfc2adb6dbb68c38a263efbd46e92bb2e
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-14 19:09:55 +0200

    Update common.hpp

--- common.hpp
+++ common.hpp
@@ -15,12 +15,11 @@ typedef uint32_t OrderId;
 constexpr OrderId InvalidOrderId = 0;
 
 typedef uint32_t Qty;
-constexpr Qty InvalidQty = 0;
 
 typedef uint32_t Price;
 constexpr Price InvalidPrice = 0;
 constexpr Price MinPrice = 1;
 constexpr Price MaxPrice = std::numeric_limits<Price>::max();
 
-typedef int32_t Index;
-constexpr Index InvalidIndex = -1;
+typedef uint32_t Index;
+constexpr Index InvalidIndex = (Index)-1;
