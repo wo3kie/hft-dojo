72e604921b3256ddd66405111e348b16caf36fed
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-19 14:07:29 +0200

    Update ../task_executor.test.cpp

---
+++ task_executor.test.cpp
@@ -0,0 +1,54 @@
+/*
+ * Project:
+ *      HFTDojo (https://github.com/wo3kie/hft-dojo)
+ *
+ * Author:
+ *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
+ */
+
+#include <cstdint>
+#include <vector>
+
+#include "./assert.hpp"
+#include "./task_executor.hpp"
+
+#include <cassert>
+#include <chrono>
+#include <iostream>
+
+void test_basic()
+{
+  TaskExecutorSPSC<4> exec;
+
+  Channel<int> channel1;
+  Channel<int> channel2;
+  Channel<int> channel3;
+  Channel<int> channel4;
+  Channel<int> channel5;
+  Channel<int> channel6;
+  Channel<int> channel7;
+  Channel<int> channel8;
+
+  Assert(exec.try_submit([](Channel<int>& channel) noexcept { channel.set(1); }, channel1));
+  Assert(exec.try_submit([](Channel<int>& channel) noexcept { channel.set(2); }, channel2));
+  Assert(exec.try_submit([](Channel<int>& channel) noexcept { channel.set(3); }, channel3));
+  Assert(exec.try_submit([](Channel<int>& channel) noexcept { channel.set(4); }, channel4));
+  exec.submit([](Channel<int>& channel) noexcept { channel.set(5); }, channel5);
+  exec.submit([](Channel<int>& channel) noexcept { channel.set(6); }, channel6);
+  exec.submit([](Channel<int>& channel) noexcept { channel.set(7); }, channel7);
+  exec.submit([](Channel<int>& channel) noexcept { channel.set(8); }, channel8);
+
+  Assert(channel1.get() == 1);
+  Assert(channel2.get() == 2);
+  Assert(channel3.get() == 3);
+  Assert(channel4.get() == 4);
+  Assert(channel5.get() == 5);
+  Assert(channel6.get() == 6);
+  Assert(channel7.get() == 7);
+  Assert(channel8.get() == 8);
+}
+
+int main()
+{
+  test_basic();
+}
