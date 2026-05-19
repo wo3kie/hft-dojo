387e536d5894a38cc84a1a7e17dcbf35bb938474
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-19 18:59:33 +0200

    Update ../task_worker.test.cpp

--- task_worker.test.cpp
+++ task_worker.test.cpp
@@ -8,11 +8,12 @@
 
 #include <functional>
 #include <iostream>
+#include <thread>
 
 #include "./assert.hpp"
 #include "./task_worker.hpp"
 
-int main()
+void test_stop_drains_all_queued_tasks()
 {
   std::size_t counter = 0;
 
@@ -29,6 +30,56 @@ int main()
   }
 
   Assert(counter == 1024);
+}
+
+void test_hard_stop_skips_queued_tasks_after_current_task()
+{
+  std::atomic<std::size_t> counter{0};
+  std::atomic<bool> current_task_started{false};
+  std::atomic<bool> release_current_task{false};
+
+  {
+    TaskWorkerSPSC<4, std::function<void()>, YieldIdlePolicy> worker;
+
+    while(! worker.push([&]() {
+      current_task_started.store(true, std::memory_order_release);
+
+      while(! release_current_task.load(std::memory_order_acquire)) {
+        std::this_thread::yield();
+      }
+
+      counter.fetch_add(1, std::memory_order_relaxed);
+    })) {
+      std::this_thread::yield();
+    }
+
+    while(! worker.push([&]() {
+      counter.fetch_add(100, std::memory_order_relaxed);
+    })) {
+      std::this_thread::yield();
+    }
+
+    while(! worker.push([&]() {
+      counter.fetch_add(1000, std::memory_order_relaxed);
+    })) {
+      std::this_thread::yield();
+    }
+
+    while(! current_task_started.load(std::memory_order_acquire)) {
+      std::this_thread::yield();
+    }
+
+    worker.hard_stop();
+    release_current_task.store(true, std::memory_order_release);
+  }
+
+  Assert(counter.load(std::memory_order_relaxed) == 1);
+}
+
+int main()
+{
+  test_stop_drains_all_queued_tasks();
+  test_hard_stop_skips_queued_tasks_after_current_task();
 
   return 0;
 }
