2d567d4e4371967d47ceb03be31ca4754125e654
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-19 13:58:18 +0200

    Update ../order_book.cpp

--- order_book.cpp
+++ order_book.cpp
@@ -10,30 +10,32 @@
 #include "./assert.hpp"
 #include "./ring_buffer_spsc.hpp"
 
-void test_order_book_insert_buy_order()
+using TestOrderBook = OrderBook<31, 32>;
+
+void test_order_book_insert_buy_order_updates_best_buy_price()
 {
   QueueOut buffer;
-  OrderBook<31, 32> ob(buffer, 100);
+  TestOrderBook ob(buffer, 100);
 
   ob.insert_buy_order(1, 100, 10);
   Assert(ob.buy_price_from() == 100);
   Assert(ob.buy_index_from() == 32);
 }
 
-void test_order_book_insert_sell_order()
+void test_order_book_insert_sell_order_updates_best_sell_price()
 {
   QueueOut buffer;
-  OrderBook<31, 32> ob(buffer, 100);
+  TestOrderBook ob(buffer, 100);
 
   ob.insert_sell_order(1, 100, 10);
   Assert(ob.sell_price_from() == 100);
   Assert(ob.sell_index_from() == 31);
 }
 
-void test_order_book_tracks_best_prices_across_levels()
+void test_order_book_tracks_best_buy_price_across_levels()
 {
   QueueOut buffer;
-  OrderBook<31, 32> ob(buffer, 100);
+  TestOrderBook ob(buffer, 100);
 
   ob.insert_buy_order(1, 99, 10);
   Assert(buffer.pop() == CreateAccepted(1, 0));
@@ -55,102 +57,217 @@ void test_order_book_tracks_best_prices_across_levels()
 
   Assert(ob.buy_price_from() == 101);
   Assert(ob.buy_index_from() == 33);
+}
+
+void test_order_book_tracks_best_sell_price_across_levels()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
+  ob.insert_buy_order(1, 99, 10);
+  Assert(buffer.pop() == CreateAccepted(1, 0));
+
+  ob.insert_buy_order(2, 101, 20);
+  Assert(buffer.pop() == CreateAccepted(2, 0));
+
+  ob.insert_buy_order(3, 100, 30);
+  Assert(buffer.pop() == CreateAccepted(3, 0));
+
+  ob.insert_sell_order(4, 101, 10);
+  Assert(buffer.pop() == CreateAccepted(4, 0));
+
+  ob.insert_sell_order(5, 99, 20);
+  Assert(buffer.pop() == CreateAccepted(5, 0));
+
+  ob.insert_sell_order(6, 100, 30);
+  Assert(buffer.pop() == CreateAccepted(6, 0));
 
   Assert(ob.sell_price_from() == 99);
   Assert(ob.sell_index_from() == 30);
 }
 
-void test_order_book_price_limits_and_range_checks()
+void test_order_book_buy_price_to_uses_book_floor()
 {
   QueueOut buffer;
-  OrderBook<31, 32> ob(buffer, 100);
+  TestOrderBook ob(buffer, 100);
 
   Assert(ob.buy_price_to() == 68);
   Assert(ob.buy_price_to(90) == 90);
   Assert(ob.buy_price_to(67) == 68);
+}
+
+void test_order_book_sell_price_to_uses_book_ceiling()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
 
   Assert(ob.sell_price_to() == 132);
   Assert(ob.sell_price_to(120) == 120);
   Assert(ob.sell_price_to(133) == 132);
+}
+
+void test_order_book_check_buy_price_accepts_values_within_range()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
 
   Assert(ob.check_buy_price(68) == true);
   Assert(ob.check_buy_price(131) == true);
+}
+
+void test_order_book_check_buy_price_rejects_values_out_of_range()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
   Assert(ob.check_buy_price(67) == false);
   Assert(ob.check_buy_price(132) == false);
+}
+
+void test_order_book_check_sell_price_accepts_values_within_range()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
 
   Assert(ob.check_sell_price(69) == true);
   Assert(ob.check_sell_price(132) == true);
+}
+
+void test_order_book_check_sell_price_rejects_values_out_of_range()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
   Assert(ob.check_sell_price(68) == false);
   Assert(ob.check_sell_price(133) == false);
 }
 
-void test_order_book_update_and_cancel_orders()
+void test_order_book_update_buy_order_accepts_matching_order()
 {
   QueueOut buffer;
-  OrderBook<31, 32> ob(buffer, 100);
+  TestOrderBook ob(buffer, 100);
 
   ob.insert_buy_order(1, 100, 10);
   Assert(buffer.pop() == CreateAccepted(1, 0));
 
   ob.update_buy_order(1, 0, 100, 15);
   Assert(buffer.pop() == UpdateAccepted(1));
+}
+
+void test_order_book_update_buy_order_rejects_unknown_order()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
+  ob.insert_buy_order(1, 100, 10);
+  Assert(buffer.pop() == CreateAccepted(1, 0));
 
   ob.update_buy_order(99, 0, 100, 15);
   Assert(buffer.pop() == UpdateRejected(99, 15));
+}
+
+void test_order_book_cancel_buy_order_rejects_unknown_order()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
+  ob.insert_buy_order(1, 100, 10);
+  Assert(buffer.pop() == CreateAccepted(1, 0));
 
   ob.cancel_buy_order(99, 0, 100);
   Assert(buffer.pop() == CancelRejected(99));
+}
+
+void test_order_book_cancel_buy_order_accepts_matching_order()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
+  ob.insert_buy_order(1, 100, 10);
+  Assert(buffer.pop() == CreateAccepted(1, 0));
 
   ob.cancel_buy_order(1, 0, 100);
   Assert(buffer.pop() == CancelAccepted(1));
+}
+
+void test_order_book_update_sell_order_accepts_matching_order()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
 
   ob.insert_sell_order(2, 100, 20);
   Assert(buffer.pop() == CreateAccepted(2, 0));
 
   ob.update_sell_order(2, 0, 100, 25);
   Assert(buffer.pop() == UpdateAccepted(2));
+}
+
+void test_order_book_update_sell_order_rejects_unknown_order()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
+  ob.insert_sell_order(2, 100, 20);
+  Assert(buffer.pop() == CreateAccepted(2, 0));
 
   ob.update_sell_order(98, 0, 100, 25);
   Assert(buffer.pop() == UpdateRejected(98, 25));
+}
+
+void test_order_book_cancel_sell_order_rejects_unknown_order()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
+  ob.insert_sell_order(2, 100, 20);
+  Assert(buffer.pop() == CreateAccepted(2, 0));
 
   ob.cancel_sell_order(98, 0, 100);
   Assert(buffer.pop() == CancelRejected(98));
+}
+
+void test_order_book_cancel_sell_order_accepts_matching_order()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
+
+  ob.insert_sell_order(2, 100, 20);
+  Assert(buffer.pop() == CreateAccepted(2, 0));
 
   ob.cancel_sell_order(2, 0, 100);
   Assert(buffer.pop() == CancelAccepted(2));
 }
 
-void test_order_book_shift_expires_out_of_range_orders()
+void test_order_book_shift_up_expires_out_of_range_sell_order()
 {
-  {
-    QueueOut buffer;
-    OrderBook<31, 32> ob(buffer, 100);
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
 
-    ob.insert_sell_order(1, 69, 10);
-    Assert(buffer.pop() == CreateAccepted(1, 0));
+  ob.insert_sell_order(1, 69, 10);
+  Assert(buffer.pop() == CreateAccepted(1, 0));
 
-    ob.shift_up();
-    Assert(buffer.pop() == OrderExpired(1));
-    Assert(ob.center_price() == 101);
-  }
+  ob.shift_up();
+  Assert(buffer.pop() == OrderExpired(1));
+  Assert(ob.center_price() == 101);
+}
 
-  {
-    QueueOut buffer;
-    OrderBook<31, 32> ob(buffer, 100);
+void test_order_book_shift_down_expires_out_of_range_buy_order()
+{
+  QueueOut buffer;
+  TestOrderBook ob(buffer, 100);
 
-    ob.insert_buy_order(2, 131, 10);
-    Assert(buffer.pop() == CreateAccepted(2, 0));
+  ob.insert_buy_order(2, 131, 10);
+  Assert(buffer.pop() == CreateAccepted(2, 0));
 
-    ob.shift_down();
-    Assert(buffer.pop() == OrderExpired(2));
-    Assert(ob.center_price() == 99);
-  }
+  ob.shift_down();
+  Assert(buffer.pop() == OrderExpired(2));
+  Assert(ob.center_price() == 99);
 }
 
 void test_order_book_shift_up()
 {
   QueueOut buffer;
-  OrderBook<31, 32> ob(buffer, 100);
+  TestOrderBook ob(buffer, 100);
 
   ob.shift_up();
   Assert(ob.center_price() == 101);
@@ -159,7 +276,7 @@ void test_order_book_shift_up()
 void test_order_book_shift_down()
 {
   QueueOut buffer;
-  OrderBook<31, 32> ob(buffer, 100);
+  TestOrderBook ob(buffer, 100);
 
   ob.shift_down();
   Assert(ob.center_price() == 99);
@@ -167,12 +284,26 @@ void test_order_book_shift_down()
 
 int main()
 {
-  test_order_book_insert_buy_order();
-  test_order_book_insert_sell_order();
-  test_order_book_tracks_best_prices_across_levels();
-  test_order_book_price_limits_and_range_checks();
-  test_order_book_update_and_cancel_orders();
-  test_order_book_shift_expires_out_of_range_orders();
+  test_order_book_insert_buy_order_updates_best_buy_price();
+  test_order_book_insert_sell_order_updates_best_sell_price();
+  test_order_book_tracks_best_buy_price_across_levels();
+  test_order_book_tracks_best_sell_price_across_levels();
+  test_order_book_buy_price_to_uses_book_floor();
+  test_order_book_sell_price_to_uses_book_ceiling();
+  test_order_book_check_buy_price_accepts_values_within_range();
+  test_order_book_check_buy_price_rejects_values_out_of_range();
+  test_order_book_check_sell_price_accepts_values_within_range();
+  test_order_book_check_sell_price_rejects_values_out_of_range();
+  test_order_book_update_buy_order_accepts_matching_order();
+  test_order_book_update_buy_order_rejects_unknown_order();
+  test_order_book_cancel_buy_order_rejects_unknown_order();
+  test_order_book_cancel_buy_order_accepts_matching_order();
+  test_order_book_update_sell_order_accepts_matching_order();
+  test_order_book_update_sell_order_rejects_unknown_order();
+  test_order_book_cancel_sell_order_rejects_unknown_order();
+  test_order_book_cancel_sell_order_accepts_matching_order();
+  test_order_book_shift_up_expires_out_of_range_sell_order();
+  test_order_book_shift_down_expires_out_of_range_buy_order();
   test_order_book_shift_up();
   test_order_book_shift_down();
 
