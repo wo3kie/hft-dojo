6d6017467418f804fb36008354e927697a4b08a0
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-20 20:20:45 +0200

    Update ../branchless.hpp

--- branchless.hpp
+++ branchless.hpp
@@ -14,42 +14,42 @@ namespace bl /* branchless */
 {
 
 template<std::signed_integral T>
-inline T min(T x, T y) noexcept
+inline T min(T x, T y)
 {
   T result = y ^ ((x ^ y) & -(x < y));
   return result;
 }
 
 template<std::unsigned_integral T>
-inline T min(T x, T y) noexcept
+inline T min(T x, T y)
 {
   T result = y ^ ((x ^ y) & -(x < y));
   return result;
 }
 
 template<std::signed_integral T>
-inline T max(T x, T y) noexcept
+inline T max(T x, T y)
 {
   T result = x ^ ((x ^ y) & -(x < y));
   return result;
 }
 
 template<std::unsigned_integral T>
-inline T max(T x, T y) noexcept
+inline T max(T x, T y)
 {
   T result = x ^ ((x ^ y) & -(x < y));
   return result;
 }
 
 template<std::signed_integral T>
-bool in_range(T x, T min, T max) noexcept
+bool in_range(T x, T min, T max)
 {
   bool result = (x >= min) && (x <= max);
   return result;
 }
 
 template<std::unsigned_integral T>
-bool in_range(T x, T min, T max) noexcept
+bool in_range(T x, T min, T max)
 {
   bool result = (x >= min) && (x <= max);
   return result;
