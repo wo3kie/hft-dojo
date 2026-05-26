6432310354e466d86e07482983a1c0826e56837d
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-26 19:07:40 +0200

    Update ../../common.hpp

--- common.hpp
+++ common.hpp
@@ -11,6 +11,28 @@
 #include <cstdint>
 #include <limits>
 
+#include <cstdlib>
+#include <cxxabi.h>
+#include <string>
+
+inline std::string demangle(const char* mangled_name)
+{
+  int status = 0;
+  char* demangled = abi::__cxa_demangle(mangled_name, nullptr, nullptr, &status);
+
+  if(status != 0) {
+    return mangled_name;
+  }
+
+  std::string result(demangled);
+  std::free(demangled);
+
+  return result;
+}
+
+#define LIKELY(x) (__builtin_expect(! ! (x), 1))
+#define UNLIKELY(x) (__builtin_expect(! ! (x), 0))
+
 typedef uint32_t OrderId;
 
 typedef uint32_t Qty;
