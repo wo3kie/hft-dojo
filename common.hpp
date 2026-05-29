210dd8e8cb3926409c266b64cd4bfceae9540671
Lukasz Czerwinski <wo3kie@gmail.com>
2026-05-29 22:18:31 +0200

    Update ../../common.hpp

--- common.hpp
+++ common.hpp
@@ -15,6 +15,7 @@
 #include <limits>
 #include <random>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 inline std::string demangle(const char* mangled_name) {
@@ -63,47 +64,72 @@ struct Request {
 
 class RequestGenerator {
 public:
-  RequestGenerator(int32_t centerPrice, int32_t levels, double laplaceScale, double marketProb, uint32_t seed = 12345)
+  RequestGenerator(int32_t centerPrice, int32_t levels, double laplaceScale, double marketProb, double cancelProb, uint32_t seed = 12345)
     : _center(centerPrice)
     , _levels(levels)
     , _laplaceScale(laplaceScale)
     , _p_Markets(marketProb)
+    , _p_Cancels(cancelProb)
     , _rng(seed)
     , _uni(0.0, 1.0)
-    , _uniSign(-0.5, 0.5) {
+    , _uniSign(-0.5, 0.5)
+  {
   }
 
-std::vector<Request> generate(size_t count) {
+std::vector<Request> generate(int32_t count) {
   std::vector<Request> out(count);
+  std::unordered_map<int32_t, int32_t> idPrice;
+  std::unordered_map<int32_t, int32_t> idSide;
 
-  for(size_t i = 0; i < count; i++) {
+  for(int32_t i = 0; i < count; i++) {
     auto &e = out[i];
-    
-    e.id = uint32_t(i + 1);
+    e.id = int32_t(i + 1);
+    double u = _uni(_rng);
+
+    if(u < _p_Cancels) {
+      e.id = std::max(1, i - int32_t(_uni(_rng) * 20));
+      e.price = idPrice[e.id] * idSide[e.id];
+      e.qty = 0;
+      continue;
+    }
+
+    if(u < _p_Cancels + _p_Markets) {
+      e.price = 0;
+      e.qty = 1 + int32_t(_uni(_rng) * 10);
+
+      if(_uni(_rng) > 0.5) {
+        e.qty = -e.qty;
+      }
+      
+      continue;
+    }
+
     e.qty = 1 + int32_t(_uni(_rng) * 10);
 
-    if(_uni(_rng) > 0.5) { 
-      e.qty = -e.qty; 
+    if(_uni(_rng) > 0.5) {
+      e.qty = -e.qty;
     }
 
-    if(_uni(_rng) < _p_Markets) {
-      e.price = 0;
-    } else {
-      double u = _uniSign(_rng);
-      double x = -_laplaceScale * ((u < 0.0) ? -1.0 : 1.0) * std::log(1.0 - 2.0 * std::abs(u));
-      int32_t price = _center + int32_t(std::round(x));
-      e.price = std::max(_center - _levels, std::min(price, _center + _levels));
-    }
+    double s = _uniSign(_rng);
+    double x = -_laplaceScale * ((s < 0.0) ? -1.0 : 1.0) * std::log(1.0 - 2.0 * std::abs(s));
+    int32_t price = _center + int32_t(std::round(x));
+
+    e.price = std::max(_center - _levels, std::min(price, _center + _levels));
+
+    idPrice[e.id] = e.price;
+    idSide[e.id] = (e.qty > 0) ? 1 : -1;
   }
 
   return out;
 }
+
 
 private:
   int32_t _center;
   int32_t _levels;
   double _laplaceScale;
   double _p_Markets;
+  double _p_Cancels;
 
   std::mt19937 _rng;
   std::uniform_real_distribution<double> _uni;
