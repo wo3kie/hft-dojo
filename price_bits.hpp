2fc642eaa53c01b4898d2d6708c3a0c0fa334a19
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-05 06:32:21 +0200

    Update ../../price_bits.hpp

---
+++ price_bits.hpp
@@ -0,0 +1,127 @@
+#pragma once
+
+/*
+ * Website:
+ *      HFTDojo https://github.com/wo3kie/hft-dojo
+ *
+ * Author:
+ *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
+ */
+
+#include <cstdint>
+
+template<size_t Bits>
+struct PriceBits final {
+  static_assert(Bits % 64 == 0);
+  static constexpr size_t Chunks = Bits / 64;
+
+  uint64_t data[Chunks]{};
+
+  void set(uint32_t bit) noexcept {
+    const size_t idx = bit >> 6;
+    const size_t off = bit & 63;
+    data[idx] |= (uint64_t(1) << off);
+  }
+
+  void clear(uint32_t bit) noexcept {
+    const size_t idx = bit >> 6;
+    const size_t off = bit & 63;
+    data[idx] &= ~(uint64_t(1) << off);
+  }
+
+  bool empty() const noexcept {
+    for(size_t i = 0; i < Chunks; ++i) {
+      if(data[i] != 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  void reset() noexcept {
+    for(size_t i = 0; i < Chunks; ++i) {
+      data[i] = 0;
+    }
+  }
+
+  uint32_t clz() const noexcept {
+    for(size_t i = Chunks; i-- > 0;) {
+      const uint64_t x = data[i];
+
+      if(x != 0) {
+        const int lz = __builtin_clzll(x);
+        const int bit_in_chunk = 63 - lz;
+        return uint32_t(i * 64 + bit_in_chunk);
+      }
+    }
+
+    assert(false);
+    return 0;
+  }
+
+  uint32_t ctz() const noexcept {
+    for(size_t i = 0; i < Chunks; ++i) {
+      const uint64_t x = data[i];
+
+      if(x != 0) {
+        const int tz = __builtin_ctzll(x);
+        const int bit_in_chunk = tz;
+        return uint32_t(i * 64 + bit_in_chunk);
+      }
+    }
+
+    assert(false);
+    return 0;
+  }
+
+  template<uint8_t N>
+  void shl() noexcept {
+    static_assert(N <= 64);
+
+    if constexpr(N == 0) {
+      return;
+    } 
+    
+    if constexpr(N < 64) {
+      for(size_t i = Chunks; i-- > 1;) {
+        const uint64_t lo = data[i - 1];
+        const uint64_t hi = data[i];
+        data[i] = (hi << N) | (lo >> (64 - N));
+      }
+
+      data[0] <<= N;
+    } else { 
+      for(size_t i = Chunks; i-- > 1;) {
+        data[i] = data[i - 1];
+      }
+
+      data[0] = 0;
+    }
+  }
+
+  template<uint8_t N>
+  void shr() noexcept {
+    static_assert(N <= 64);
+
+    if constexpr(N == 0) {
+      return;
+    } 
+    
+    if constexpr(N < 64) {
+      for(size_t i = 0; i < Chunks - 1; ++i) {
+        const uint64_t lo = data[i];
+        const uint64_t hi = data[i + 1];
+        data[i] = (lo >> N) | (hi << (64 - N));
+      }
+
+      data[Chunks - 1] >>= N;
+    } else {
+      for(size_t i = 0; i < Chunks - 1; ++i) {
+        data[i] = data[i + 1];
+      }
+
+      data[Chunks - 1] = 0;
+    }
+  }
+};
