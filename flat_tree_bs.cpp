d9eb74767946014a5ea8c9ff68ae2a411ed76c14
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-10 18:42:27 +0200

    Update ../../flat_tree_bs.cpp

--- flat_tree_bs.cpp
+++ flat_tree_bs.cpp
@@ -6,64 +6,32 @@
  *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
  */
 
-#include <algorithm>
-#include <set>
-#include <unordered_set>
-
-#include "assert.hpp"
 #include "flat_tree_bs.hpp"
-#include "random.hpp"
 #include "test_utils.hpp"
-#include "timer.hpp"
-
-template<unsigned N>
-void test_tree_bs() {
-  LCG lcg;
-
-  std::set<int>set;
-  FlatTreeBS<int, N> tree;
-
-  std::vector<int> values;
-
-  for(int i = 0; i < N; ++i) {
-    values.push_back(i);
-  }
-
-  std::shuffle(values.begin(), values.end(), lcg);
-  
-  for(int i = 0; i < N; ++i) {
-    Assert(tree.insert(values[i]) != -1);
-    Assert(set.insert(values[i]).second);
-
-    Assert(std::equal(set.begin(), set.end(), std::begin(tree), std::end(tree)));
-  }
-  
-  std::shuffle(values.begin(), values.end(), lcg);
-
-  for(int i = 0; i < N; ++i) {
-    Assert(tree.find(values[i]) != -1);
-  }
-  
-  std::shuffle(values.begin(), values.end(), lcg);
-
-  for(int i = 0; i < N; ++i) {
-    Assert(tree.erase(tree.find(values[i])));
-    Assert(set.erase(values[i]) == 1);
-    Assert(std::equal(set.begin(), set.end(), std::begin(tree), std::end(tree)));
-  }
-}
 
 int main() {
-
   for(unsigned i = 0; i < 1024; ++i) {
-    test_tree_bs<1>();
-    test_tree_bs<2>();
-    test_tree_bs<4>();
-    test_tree_bs<8>();
-    test_tree_bs<16>();
-    test_tree_bs<32>();
-    test_tree_bs<64>();
+    test_flat_tree<FlatTreeBS<int, 1>>();
+    test_flat_tree<FlatTreeBS<int, 2>>();
+    test_flat_tree<FlatTreeBS<int, 4>>();
+    test_flat_tree<FlatTreeBS<int, 8>>();
+    test_flat_tree<FlatTreeBS<int, 16>>();
+    test_flat_tree<FlatTreeBS<int, 32>>();
+    test_flat_tree<FlatTreeBS<int, 64>>();
   }
+
+#ifdef NDEBUG
+  bench_flat_tree<FlatTreeBS<int, 1024>>(1'000'000);
+  bench_flat_tree<std::set<int>>(1'000'000);
+#else
+  /* stack storage */
+  bench_flat_tree<FlatTreeBS<int, 1024>>(1'000);
+  bench_flat_tree<std::set<int>>(1'000);
+
+  /* heap storage */
+  bench_flat_tree<FlatTreeBS<int, 1024>>(10'000);
+  bench_flat_tree<std::set<int>>(10'000);
+#endif
 
   return 0;
 }
