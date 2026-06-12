b70bc488a66a5b0561fd3a3d2ce2fa08c2a700d9
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-12 21:42:14 +0200

    Update ../../flat_list.cpp

--- flat_list.cpp
+++ flat_list.cpp
@@ -10,7 +10,7 @@
 #include "test_utils.hpp"
 
 int main() {
-  for(unsigned i = 0; i < 1024; ++i) {
+  for(int32_t i = 0; i < 1024; ++i) {
     test_flat_list<FlatList<int, 1>>();
     test_flat_list<FlatList<int, 2>>();
     test_flat_list<FlatList<int, 4>>();
