f027eb56364af634d61a2b9b57414d325e2b778d
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-12 08:56:18 +0200

    Update ../../flat_list.cpp

--- flat_list.cpp
+++ flat_list.cpp
@@ -6,8 +6,6 @@
 *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
 */
 
-#include <list>
-
 #include "flat_list.hpp"
 #include "test_utils.hpp"
 
@@ -28,11 +26,6 @@ int main() {
   bench_flat_list<FlatList<int, 100'000>>(100'000, "FlatList");
   bench_flat_list<FlatList<int, 100'000>>(100'000, "FlatList");
   bench_flat_list<FlatList<int, 100'000>>(100'000, "FlatList");
-  bench_flat_list<std::list<int>>(100'000, "std::list");
-  bench_flat_list<std::list<int>>(100'000, "std::list");
-  bench_flat_list<std::list<int>>(100'000, "std::list");
-  bench_flat_list<std::list<int>>(100'000, "std::list");
-  bench_flat_list<std::list<int>>(100'000, "std::list");
 #else
   bench_flat_list<FlatList<int, 10'000>>(10'000, "FlatList");
   bench_flat_list<std::list<int>>(10'000, "std::list");
