f5f235d43f8a8109c58d06106acddb8b1404a6e8
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-12 21:42:14 +0200

    Update ../../flat_hash.cpp

--- flat_hash.cpp
+++ flat_hash.cpp
@@ -3,6 +3,17 @@
 #include "test_utils.hpp"
 
 int main(){
+  for (int i = 0; i < 1024; ++i) {
+    test_flat_hash<FlatHash<int, int, 8>>(8);
+    test_flat_hash<FlatHash<int, int, 16>>(16);
+    test_flat_hash<FlatHash<int, int, 32>>(32);
+    test_flat_hash<FlatHash<int, int, 64>>(64);
+    test_flat_hash<FlatHash<int, int, 128>>(128);
+    test_flat_hash<FlatHash<int, int, 256>>(256);
+    test_flat_hash<FlatHash<int, int, 512>>(512);
+    test_flat_hash<FlatHash<int, int, 1024>>(1024);
+  }
+
 #ifdef NDEBUG
   bench_flat_hash<FlatHash<int, int, 128 * 1024>>(128 * 1024, "FlatHash");
   bench_flat_hash<FlatHash<int, int, 128 * 1024>>(128 * 1024, "FlatHash");
