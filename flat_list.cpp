bac95b58acbd3d8fcd7e75e145d6edcbe8d804c2
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-15 21:47:08 +0200

    Update ../../flat_list.cpp

--- flat_list.cpp
+++ flat_list.cpp
@@ -13,105 +13,146 @@
 
 namespace {
 
-void test_empty_list() {
-	FlatList<int, 4> list;
-
-	assert(list.capacity() == 4);
-	assert(list._ext_equal({}));
-
-	assert(list.find(99) == -1);
-}
-
 void test_push_front_and_back() {
 	FlatList<int, 5> list;
 
-	const int32_t front_slot = list.push_front(20);
-	assert(front_slot != -1);
+	list.push_front(20);
 	assert(list._ext_equal({20}));
 
-	const int32_t back_slot = list.push_back(30);
-	assert(back_slot != -1);
+	list.push_back(30);
 	assert(list._ext_equal({20, 30}));
 
 	list.push_front(10);
 	assert(list._ext_equal({10, 20, 30}));
 
-	assert(list.find(10) != -1);
-	assert(list.find(20) == front_slot);
-	assert(list.find(30) == back_slot);
-	assert(list[front_slot] == 20);
-	assert(list[back_slot] == 30);
-}
-
-void test_insert_and_erase_middle() {
-	FlatList<int, 6> list;
-
-	const int32_t slot20 = list.push_back(20);
-	const int32_t slot40 = list.push_back(40);
-	assert(list._ext_equal({20, 40}));
-
-	const int32_t slot30 = list.insert(slot40, 30);
-	assert(slot30 != -1);
-	assert(list._ext_equal({20, 30, 40}));
-
-	const int32_t slot10 = list.insert(slot20, 10);
-	assert(slot10 != -1);
+	list.push_back(40);
 	assert(list._ext_equal({10, 20, 30, 40}));
 
-	const int32_t slot50 = list.insert(-1, 50);
-	assert(slot50 != -1);
-	assert(list._ext_equal({10, 20, 30, 40, 50}));
+	list.push_front(0);
+	assert(list._ext_equal({0, 10, 20, 30, 40}));
 
-	list.erase(slot30);
-	assert(list._ext_equal({10, 20, 40, 50}));
-	assert(list.find(30) == -1);
-
-	list.erase(slot10);
-	assert(list._ext_equal({20, 40, 50}));
-
-	list.erase(slot50);
-	assert(list._ext_equal({20, 40}));
-
-	const int32_t reused = list.push_back(60);
-	assert(reused == slot50 || reused == slot30 || reused == slot10);
-	assert(list._ext_equal({20, 40, 60}));
+	assert(list.full());
 }
 
 void test_pop_front_and_back() {
-	FlatList<int, 4> list;
+	FlatList<int, 5> list;
 
-	list.push_back(1);
-	list.push_back(2);
-	list.push_back(3);
-	assert(list._ext_equal({1, 2, 3}));
+	list.push_back(10);
+	list.push_back(20);
+	list.push_back(30);
+	list.push_back(40);
+	list.push_back(50);
+	assert(list._ext_equal({10, 20, 30, 40, 50}));
+	
+	list.pop_front();
+	assert(list._ext_equal({20, 30, 40, 50}));
+
+	list.pop_back();
+	assert(list._ext_equal({20, 30, 40}));
 
 	list.pop_front();
-	assert(list._ext_equal({2, 3}));
+	assert(list._ext_equal({30, 40}));
 
-	list.pop_back();
-	assert(list._ext_equal({2}));
+	list.pop_front();
+	assert(list._ext_equal({40}));
 
-	list.pop_back();
+	list.pop_front();
 	assert(list._ext_equal({}));
 }
 
-void test_fill_list() {
-	FlatList<int, 3> list;
+void test_insert_middle() {
+	FlatList<int, 5> list;
 
-	list.push_back(7);
-	list.push_back(8);
-	list.push_back(9);
+	list.insert(-1, 40);
+	assert(list._ext_equal({40}));
 
-	assert(list._ext_equal({7, 8, 9}));
-	assert(list.full());
+	list.insert(list.find(40), 20);
+	assert(list._ext_equal({20, 40}));
+
+	list.insert(list.find(40), 30);
+	assert(list._ext_equal({20, 30, 40}));
+
+	list.insert(list.find(20), 10);
+	assert(list._ext_equal({10, 20, 30, 40}));
+
+	list.insert(-1, 50);
+	assert(list._ext_equal({10, 20, 30, 40, 50}));
+}
+
+void test_erase_middle() {
+	FlatList<int, 5> list;
+
+	list.push_back(10);
+	list.push_back(20);
+	list.push_back(30);
+	list.push_back(40);
+	list.push_back(50);
+	assert(list._ext_equal({10, 20, 30, 40, 50}));
+	
+	list.erase(list.find(10));
+	assert(list._ext_equal({20, 30, 40, 50}));
+
+	list.erase(list.find(50));
+	assert(list._ext_equal({20, 30, 40}));
+
+	list.erase(list.find(30));
+	assert(list._ext_equal({20, 40}));
+
+	list.erase(list.find(20));
+	assert(list._ext_equal({40}));
+
+	list.erase(list.find(40));
+	assert(list._ext_equal({}));
+}
+
+void test_reuse_slots() {
+	FlatList<int, 5> list;
+
+	list.push_back(10);
+	list.push_back(20);
+	list.push_back(30);
+	list.push_back(40);
+	list.push_back(50);
+	assert(list._ext_equal({10, 20, 30, 40, 50}));
+	
+	list.erase(list.find(30));
+	assert(list._ext_equal({10, 20, 40, 50}));
+
+	list.insert(list.find(40), 35);
+	assert(list._ext_equal({10, 20, 35, 40, 50}));
+
+	list.erase(list.find(10));
+	assert(list._ext_equal({20, 35, 40, 50}));
+
+	list.insert(list.find(20), 15);
+	assert(list._ext_equal({15, 20, 35, 40, 50}));
+
+	list.erase(list.find(50));
+	assert(list._ext_equal({15, 20, 35, 40}));
+
+	list.insert(-1, 55);
+	assert(list._ext_equal({15, 20, 35, 40, 55}));
+
+	list.erase(list.find(20));
+	assert(list._ext_equal({15, 35, 40, 55}));
+
+	list.insert(list.find(35), 25);
+	assert(list._ext_equal({15, 25, 35, 40, 55}));
+
+	list.erase(list.find(40));
+	assert(list._ext_equal({15, 25, 35, 55}));
+
+	list.insert(list.find(55), 45);
+	assert(list._ext_equal({15, 25, 35, 45, 55}));
 }
 
 } // namespace
 
 int main() {
-	test_empty_list();
 	test_push_front_and_back();
-	test_insert_and_erase_middle();
 	test_pop_front_and_back();
-	test_fill_list();
+
+	test_insert_middle();
+	test_erase_middle();
+	test_reuse_slots();
 }
