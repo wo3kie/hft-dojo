52b392f9f872bc6ee338f7a0a7172df8ff93c515
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-15 14:13:05 +0200

    Update ../../object_pool.hpp

--- object_pool.hpp
+++ object_pool.hpp
@@ -22,6 +22,8 @@ template<typename T, int32_t Capacity>
   requires std::is_trivially_destructible_v<T>
 struct ObjectPool: noncopyable, nonmovable {
 public:
+  static_assert((Capacity > 0) && (Capacity <= 1024 * 1024 * 1024));
+
   using index_type = index_type_t<Capacity>;
 
   static constexpr int32_t npos = -1;
@@ -29,7 +31,7 @@ public:
   ObjectPool() {
     _size = 0;
 
-    for(int32_t i = 0; i < Capacity; ++i) {
+    for(index_type i = 0; i < (index_type)Capacity; ++i) {
       _free[i] = i;
     }
   }
@@ -42,28 +44,28 @@ public:
   }
 
   int32_t size() const noexcept {
-    return _size;
+    return (int32_t)_size;
   }
 
-  bool empty() const noexcept {
+  [[nodiscard]] bool empty() const noexcept {
     return _size == 0;
   }  
 
   bool full() const noexcept {
-    return _size == Capacity;
+    return _size == (index_type)Capacity;
   }  
 
   int32_t allocate() noexcept {
     if(full()) {
-      return npos;
+      return (int32_t)npos;
     }
 
-    return _free[_size++];
+    return (int32_t)_free[_size++];
   }
 
   template<typename... Args>
   int32_t allocate(Args&&... args) noexcept {
-    int32_t slot = allocate();
+    const int32_t slot = allocate();
    
     if(slot != npos) {
       new(&_buffer[slot]) T(std::forward<Args>(args)...);
@@ -74,22 +76,21 @@ public:
 
   void deallocate(int32_t slot) noexcept {
     assert(empty() == false);
-    assert(slot >= 0);
-    assert(slot < Capacity);
+    assert((slot >= 0) && (slot < Capacity));
     
     _free[--_size] = (index_type)slot;
   }
-
+  
   T& operator[](int32_t slot) noexcept {
-    assert(slot >= 0);
-    assert(slot < Capacity);
-
+    assert(empty() == false);
+    assert((slot >= 0) && (slot < Capacity));
+    
     return _buffer[slot];
   }
-
+  
   const T& operator[](int32_t slot) const noexcept {
-    assert(slot >= 0);
-    assert(slot < Capacity);
+    assert(empty() == false);
+    assert((slot >= 0) && (slot < Capacity));
 
     return _buffer[slot];
   }
