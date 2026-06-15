9371383ec9f4d250f319e4b80f2ff42a6f85841e
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-15 11:53:07 +0200

    Update ../../flat_queue.hpp

--- flat_queue.hpp
+++ flat_queue.hpp
@@ -10,6 +10,7 @@
 
 #include <cassert>
 #include <cstdint>
+#include <list>
 #include <type_traits>
 
 #include "assert.hpp"
@@ -21,18 +22,23 @@
  */
 
 template<typename Value, int32_t Capacity>
-class FlatQueue {
+struct FlatQueue {
+public:
   using index_type = index_type_t<Capacity>;
 
+public:
   static constexpr index_type Sentinel = Capacity;
+  static constexpr index_type Free = -1;
   
 public:
   FlatQueue() {
     for(int32_t i = 0; i < Capacity + 1; i += 1) {
-      _buffer[i]._next = Sentinel;
-      _buffer[i]._prev = Sentinel;
+      _buffer[i]._next = Free;
+      _buffer[i]._prev = Free;
     }
 
+    _buffer[Sentinel]._prev = Sentinel;
+    _buffer[Sentinel]._next = Sentinel;
     _buffer[Sentinel]._size = 0;
   }
 
@@ -40,16 +46,58 @@ public:
     return Capacity;
   }
 
+  int32_t size() const noexcept {
+    return _buffer[Sentinel]._size;
+  }
+
   bool empty() const noexcept {
-    return _buffer[Sentinel]._size == 0;
+    return size() == 0;
   }
 
   bool full() const noexcept {
-    return _buffer[Sentinel]._size == Capacity;
+    return size() == Capacity;
   }
 
   Value& front() noexcept {
     return _buffer[_buffer[Sentinel]._next]._value;
+  }
+
+  void push(int32_t slot, const Value& value) noexcept {
+    assert(slot >= 0 && slot < Sentinel);
+    assert(_ext_is_free(slot));
+
+    const index_type prev = _buffer[Sentinel]._prev;
+    const index_type next = Sentinel;
+
+    slot = _allocate(slot, value, prev, next);
+    
+    _buffer[prev]._next = slot;
+    _buffer[Sentinel]._prev = slot;
+  }
+
+  void erase(int32_t slot) noexcept {
+    assert(slot >= 0 && slot < Sentinel);
+    assert(! _ext_is_free(slot));
+
+    const index_type prev = _buffer[slot]._prev;
+    const index_type next = _buffer[slot]._next;
+
+    _buffer[prev]._next = next;
+    _buffer[next]._prev = prev;
+
+    _deallocate(slot);
+  }
+
+  void pop() noexcept {
+    assert(! _ext_is_free(_buffer[Sentinel]._next));
+
+    const index_type head = _buffer[Sentinel]._next;
+    const index_type next = _buffer[head]._next;
+
+    _buffer[next]._prev = Sentinel;
+    _buffer[Sentinel]._next = next;
+
+    _deallocate(head);
   }
 
   Value& operator[](int32_t slot) noexcept {
@@ -60,44 +108,38 @@ public:
     return _buffer[slot]._value;
   }
 
-  void push(int32_t slot, const Value& value) noexcept {
+  int32_t find(const Value& value) const noexcept {
+    for(index_type slot = _buffer[Sentinel]._next; slot != Sentinel; slot = _buffer[slot]._next) {
+      if(_buffer[slot]._value == value) {
+        return slot;
+      }
+    }
+
+    return -1;
+  }
+
+  /* extension */ bool _ext_is_free(int32_t slot) const noexcept {
     assert(slot >= 0 && slot < Sentinel);
-
-    slot = _allocate(slot);
-    _buffer[Sentinel]._size += 1;
-
-    _Node& node = _buffer[slot];
-    node._value = value;
-
-    const index_type tail = _buffer[Sentinel]._prev;
-
-    node._prev = tail;
-    node._next = Sentinel;
-
-    _buffer[tail]._next = slot;
-    _buffer[Sentinel]._prev = slot;
+    return (_buffer[slot]._next == Free) && (_buffer[slot]._prev == Free);
   }
 
-  void remove(int32_t slot) noexcept {
-    const index_type prev = _buffer[slot]._prev;
-    const index_type next = _buffer[slot]._next;
+  /* extension */ bool _ext_equal(std::list<Value> expected) const noexcept {
+    if (this->size() != expected.size()) {
+      return false;
+    }
 
-    _buffer[prev]._next = next;
-    _buffer[next]._prev = prev;
+    for(index_type slot = _buffer[Sentinel]._next; slot != Sentinel; slot = _buffer[slot]._next) {
+      assert(_buffer[_buffer[slot]._prev]._next == slot);
+      assert(_buffer[_buffer[slot]._next]._prev == slot);
 
-    _deallocate(slot);
-    _buffer[Sentinel]._size -= 1;
-  }
+      if(_buffer[slot]._value != expected.front()) {
+        return false;
+      }
 
-  void pop() noexcept {
-    const index_type head = _buffer[Sentinel]._next;
-    const index_type next = _buffer[head]._next;
+      expected.pop_front();
+    }
 
-    _buffer[next]._prev = Sentinel;
-    _buffer[Sentinel]._next = next;
-
-    _deallocate(head);
-    _buffer[Sentinel]._size -= 1;
+    return expected.empty();
   }
 
 private:
@@ -109,18 +151,23 @@ private:
   };
 
 private:
-  int32_t _allocate(int32_t slot) noexcept {
-    assert(! full());
+  int32_t _allocate(int32_t slot, const Value& value, int32_t prev, int32_t next) noexcept {
     assert(slot >= 0 && slot < Sentinel);
-    assert(_buffer[slot]._prev == Sentinel);
-    assert(_buffer[slot]._next == Sentinel);
+    assert(_buffer[slot]._prev == Free);
+    assert(_buffer[slot]._next == Free);
+
+    _buffer[slot]._value = value;
+    _buffer[slot]._prev = prev;
+    _buffer[slot]._next = next;
+    _buffer[Sentinel]._size += 1;
 
     return slot;
   }
 
   void _deallocate(int32_t slot) noexcept {
-    _buffer[slot]._next = Sentinel;
-    _buffer[slot]._prev = Sentinel;
+    _buffer[slot]._next = Free;
+    _buffer[slot]._prev = Free;
+    _buffer[Sentinel]._size -= 1;
   }
 
 private:
