04cc9f233ce5b5b78034e624928859f9688ef0e5
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-15 21:47:08 +0200

    Update ../../ring_buffer_mutex.cpp

--- ring_buffer_mutex.cpp
+++ ring_buffer_mutex.cpp
@@ -1,127 +1,67 @@
 /*
- * Website:
- *      HFTDojo https://github.com/wo3kie/hft-dojo
+ * Project:
+ *      HFTDojo (https://github.com/wo3kie/hft-dojo)
  *
  * Author:
  *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
+ *
  */
 
+#include <stdexcept>
+#include <utility>
+#include <queue>
+
 #include "assert.hpp"
+#include "common.hpp"
+
 #include "ring_buffer_mutex.hpp"
 #include "test_utils.hpp"
 
 namespace {
 
-struct MoveOnly {
-	int value = 0;
+template<int32_t Capacity>
+void test_push_pop() {
+  RingBufferMutex<int, Capacity> actual;
+  std::queue<int> expected;
 
-	MoveOnly() = default;
-	explicit MoveOnly(int value): value(value) {
-	}
+  for(int32_t i = 0; i < Capacity / 2; i += 1) {
+    actual.push(i);
+    expected.push(i);
+    assert(actual._ext_equal(expected));
+  }
 
-	MoveOnly(const MoveOnly&) = delete;
-	MoveOnly& operator=(const MoveOnly&) = delete;
+  for(int32_t i = 0; i < Capacity; i += 1) {
+    int out;
 
-	MoveOnly(MoveOnly&& other) noexcept: value(other.value) {
-		other.value = -1;
-	}
+    actual.push(i);
+    expected.push(i);
+    assert(actual._ext_equal(expected));
 
-	MoveOnly& operator=(MoveOnly&& other) noexcept {
-		if(this != &other) {
-			value = other.value;
-			other.value = -1;
-		}
+    actual.pop(out);
+    expected.pop();
+    assert(actual._ext_equal(expected));
+  }
 
-		return *this;
-	}
-};
+  for(int32_t i = 0; i < Capacity / 2; i += 1) {
+    int out;
 
-template<std::size_t Capacity>
-void test_empty_full_and_fifo_without_blocking() {
-	RingBufferMutex<int, Capacity> buffer;
-
-	Assert(buffer.capacity() == Capacity);
-	Assert(buffer.empty() == true);
-	Assert(buffer.full() == false);
-
-	for(std::size_t i = 0; i < Capacity; ++i) {
-		Assert(buffer.push(static_cast<int>(i + 10)) == true);
-	}
-
-	Assert(buffer.empty() == false);
-	Assert(buffer.full() == true);
-
-	int out = -1;
-
-	for(std::size_t i = 0; i < Capacity; ++i) {
-		Assert(buffer.pop(out) == true);
-		Assert(out == static_cast<int>(i + 10));
-	}
-
-	Assert(buffer.empty() == true);
-	Assert(buffer.full() == false);
-}
-
-template<std::size_t Capacity>
-void test_wrap_around_without_blocking() {
-	RingBufferMutex<int, Capacity> buffer;
-	int out = -1;
-
-	for(std::size_t i = 0; i < Capacity; ++i) {
-		Assert(buffer.push(static_cast<int>(i)) == true);
-	}
-
-	for(std::size_t i = 0; i < Capacity / 2; ++i) {
-		Assert(buffer.pop(out) == true);
-		Assert(out == static_cast<int>(i));
-	}
-
-	for(std::size_t i = 0; i < Capacity / 2; ++i) {
-		Assert(buffer.push(static_cast<int>(100 + i)) == true);
-	}
-
-	for(std::size_t i = Capacity / 2; i < Capacity; ++i) {
-		Assert(buffer.pop(out) == true);
-		Assert(out == static_cast<int>(i));
-	}
-
-	for(std::size_t i = 0; i < Capacity / 2; ++i) {
-		Assert(buffer.pop(out) == true);
-		Assert(out == static_cast<int>(100 + i));
-	}
-
-	Assert(buffer.empty() == true);
-}
-
-void test_ext_pop_on_non_empty_buffer() {
-	RingBufferMutex<int, 4> buffer;
-
-	Assert(buffer.push(42) == true);
-	Assert(buffer._ext_pop() == 42);
-	Assert(buffer.empty() == true);
-}
-
-void test_move_only_values() {
-	RingBufferMutex<MoveOnly, 4> buffer;
-
-	MoveOnly pushed(42);
-	Assert(buffer.push(std::move(pushed)) == true);
-	Assert(pushed.value == -1);
-
-	MoveOnly out;
-	Assert(buffer.pop(out) == true);
-	Assert(out.value == 42);
-	Assert(buffer.empty() == true);
+    actual.pop(out);
+    expected.pop();
+    assert(actual._ext_equal(expected));
+  }
 }
 
 } // namespace
 
-int main() {
-	test_empty_full_and_fifo_without_blocking<3>();
-	test_empty_full_and_fifo_without_blocking<4>();
-	test_wrap_around_without_blocking<4>();
-	test_wrap_around_without_blocking<5>();
-	test_ext_pop_on_non_empty_buffer();
-	test_move_only_values();
+/*
+ * main
+ */
 
+int main() {
+  test_push_pop<8>();
+  test_push_pop<15>();
+  test_push_pop<16>();
+  test_push_pop<17>();
+  test_push_pop<1023>();
+  test_push_pop<1024>();
 }
