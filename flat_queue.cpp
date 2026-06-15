cc5196399cb223647b97d33758470c9e207d2db7
Lukasz Czerwinski <wo3kie@gmail.com>
2026-06-15 11:53:07 +0200

    Update ../../flat_queue.cpp

--- flat_queue.cpp
+++ flat_queue.cpp
@@ -7,45 +7,75 @@
 */
 
 #include "flat_queue.hpp"
-#include "test_utils.hpp"
+#include <cassert>
+#include <list>
 
-void test_flat_queue_oa() {
+namespace {
+
+void test_empty_queue() {
   FlatQueue<int, 4> queue;
-  assert(queue.empty());
 
-  queue.push(0, 1);
-  assert(queue.empty() == false);
-
-  queue.push(1, 2);
-  queue.push(2, 3);
-  queue.push(3, 4);
-  assert(queue.full());
-
-  assert(queue.front() == 1);
-  queue.pop();
-
-  assert(queue.front() == 2);
-  queue.pop();
-
-  assert(queue.front() == 3);
-  queue.pop();
-
-  assert(queue.front() == 4);
-  queue.pop();
-
-  assert(queue.empty());
+  assert(queue.capacity() == 4);
+  queue._ext_equal({});
+  assert(queue.find(42) == -1);
 }
 
-int main() {
-  test_flat_queue_oa();
+void test_push_pop_fifo_order() {
+  FlatQueue<int, 4> queue;
 
-#ifdef NDEBUG
-  bench_flat_queue<FlatQueue<int, 100'000>>("FlatQueue");
-  bench_flat_queue<FlatQueue<int, 100'000>>("FlatQueue");
-  bench_flat_queue<FlatQueue<int, 100'000>>("FlatQueue");
-  bench_flat_queue<FlatQueue<int, 100'000>>("FlatQueue");
-  bench_flat_queue<FlatQueue<int, 100'000>>("FlatQueue");
-#else
-  bench_flat_queue<FlatQueue<int, 10'000>>("FlatQueue");
-#endif
+  queue.push(2, 20);
+  queue._ext_equal({20});
+
+  queue.push(0, 10);
+  queue._ext_equal({20, 10});
+
+  queue.push(3, 30);
+  queue._ext_equal({20, 10, 30});
+
+  queue.pop();
+  queue._ext_equal({10, 30});
+
+  queue.pop();
+  queue._ext_equal({30});
+
+  queue.pop();
+  queue._ext_equal({});
+}
+
+void test_erase_middle_and_reuse_slot() {
+  FlatQueue<int, 5> queue;
+
+  queue.push(4, 40);
+  queue.push(1, 10);
+  queue.push(3, 30);
+  queue.push(0, 0);
+  queue._ext_equal({40, 10, 30, 0});
+
+  queue.erase(queue.find(10));
+  queue._ext_equal({40, 30, 0});
+
+  queue.push(1, 10);
+  queue._ext_equal({40, 30, 0, 10});
+
+  queue.erase(queue.find(40));
+  queue._ext_equal({30, 0, 10});
+
+  queue.erase(queue.find(10));
+  queue._ext_equal({30, 0});
+
+  queue.erase(queue.find(0));
+  queue._ext_equal({30});
+
+  queue.erase(queue.find(30));
+  queue._ext_equal({});
+}
+
+} // namespace
+
+int main() {
+  test_empty_queue();
+  test_push_pop_fifo_order();
+  test_erase_middle_and_reuse_slot();
+
+  return 0;
 }
