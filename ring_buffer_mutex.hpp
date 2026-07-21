9a96df692911f1ae17a9877d88d863b4b8147e84
Lukasz Czerwinski <wo3kie@gmail.com>
2026-07-21 21:26:44 +0200

    Update ../../ring_buffer_mutex.hpp

--- ring_buffer_mutex.hpp
+++ ring_buffer_mutex.hpp
@@ -1,14 +1,11 @@
 #pragma once
 
 /*
- * Project:
- *      HFTDojo (https://github.com/wo3kie/hft-dojo)
- *
- * Author:
- *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
+ * Author: Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
  */
 
 #include <condition_variable>
+#include <queue>
 #include <mutex>
 
 #include "common.hpp"
@@ -18,7 +15,7 @@
  * RingBufferMutex - Ring Buffer MuTex
  */
 
-template<typename TValue, int32_t Capacity>
+template<typename TValue, std::size_t Capacity>
 struct RingBufferMutex : noncopyable, nonmovable {
   static_assert((Capacity > 0) && (Capacity <= 1024 * 1024 * 1024));
 
@@ -26,61 +23,60 @@ public:
   using value_type = TValue;
 
 public:
-  RingBufferMutex() = default;
-  ~RingBufferMutex() = default;
-
-public:
-  static constexpr std::size_t capacity() {
+  static constexpr std::size_t capacity() noexcept {
     return Capacity;
   }
 
-  int32_t size() const {
+  std::size_t size() const noexcept {
     std::lock_guard<std::mutex> lock(_mutex);
     return _buffer.size();
   }
 
-  [[nodiscard]] bool empty() const {
+  [[nodiscard]] bool empty() const noexcept {
     std::lock_guard<std::mutex> lock(_mutex);
     return _buffer.empty();
   }
 
-  bool full() const {
+  bool full() const noexcept {
     std::lock_guard<std::mutex> lock(_mutex);
     return _buffer.full();
   }
 
   template<typename TT>
-  bool push(TT&& t) {
+  void push(TT&& t) noexcept {
     std::unique_lock<std::mutex> lock(_mutex);
 
     _notFull.wait(lock, [this]() {
-      return ! this->_buffer.full();
+      return this->_buffer.full() == false;
     });
 
     _buffer.push(std::forward<TT>(t));
     _notEmpty.notify_one();
-
-    return true;
   }
 
-  bool pop(TValue& out) {
+  void pop(TValue& out) noexcept {
     std::unique_lock<std::mutex> lock(_mutex);
 
     _notEmpty.wait(lock, [this]() {
-      return ! this->_buffer.empty();
+      return this->_buffer.empty() == false;
     });
 
     _buffer.pop(out);
     _notFull.notify_one();
-
-    return true;
   }
 
   /* extension */ TValue _ext_pop() {
     TValue out;
 
-    if(! pop(out)) {
-      throw std::runtime_error("RingBufferMutex is empty");
+    {
+      std::unique_lock<std::mutex> lock(_mutex);
+      
+      if (empty()) {
+        throw std::runtime_error("RingBuffer is empty");
+      }
+      
+      _buffer.pop(out);
+      _notFull.notify_one();
     }
 
     return out;
