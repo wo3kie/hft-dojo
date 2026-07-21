39a1b448cd9a07333c652ced3e741c933c356268
Lukasz Czerwinski <wo3kie@gmail.com>
2026-07-21 21:26:44 +0200

    Update ../../storage.hpp

--- storage.hpp
+++ storage.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
 /*
- * Website:
- *      HFTDojo https://github.com/wo3kie/hft-dojo
- *
- * Author:
- *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
+ * Author: Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
  */
 
 #include <cstddef>
