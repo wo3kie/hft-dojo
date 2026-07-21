cc4ac95112b61f8dfc0d22b07887cf6f7042d40e
Lukasz Czerwinski <wo3kie@gmail.com>
2026-07-21 21:26:44 +0200

    Update ../../flat_tree_avl.cpp

--- flat_tree_avl.cpp
+++ flat_tree_avl.cpp
@@ -1,9 +1,5 @@
 /*
- * Project:
- *      HFTDojo (https://github.com/wo3kie/hft-dojo)
- *
- * Author:
- *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
+ * Author: Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
  */
 
 #include "flat_tree_avl.hpp"
